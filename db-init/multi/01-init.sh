9ced6397f29e6ba6e9f6073a560666095ab040e8
molktru13 <molktru@gmail.com>
2026-08-12 11:50:09 -0300

    Add files via upload

--- db-init/multi/01-init.sh
+++ db-init/multi/01-init.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
 
+echo ">>> Aplicando init.sql no banco flags_db..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /init/flags.sql
+
+echo ">>> Criando banco targeting_db..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE targeting_db;"
+
+echo ">>> Aplicando init.sql no banco targeting_db..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "targeting_db" -f /init/targeting.sql
+
+echo ">>> Init multi-banco concluído."
